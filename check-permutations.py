75382c2f3fa31dd99997cfba202ab1ce8af1f937
Robbie <robbiet@live.unc.edu>
2021-07-03 11:17:40 -0500

    First commit of CTCI ch1

--- check-permutations.py
+++ check-permutations.py
@@ -13,6 +13,7 @@ def check_permutations(base, second):
                 return(print("not a perm"))
             if b == a:
                 print(new_base)
+                #
                 new_base = base.replace(a, "")
                 print(new_base)
                 new_second = second.replace(b, "")
